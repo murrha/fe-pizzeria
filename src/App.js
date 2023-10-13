ba9024ec9a30d58f8059a43c9a9c8eabeddafde9
George Saito <gsaito12600@gmail.com>
2023-10-13 09:10:55 -0700

    Adding test comments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
+// Adding test comments
+// Adding test comments
+// Adding test comments
+
 import "./App.css";
-import Media from 'react-media';
-import Header from './components/Header';
-import MdHeader from './components/MdHeader'
+import Media from "react-media";
+import Header from "./components/Header";
+import MdHeader from "./components/MdHeader";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <div className="container-fluid">
-    <Media query={{ maxWidth: 599 }}>
-          {matches =>
-            matches ? (
-             <MdHeader/>
-            ) : (
-              <Header/>
-            )
-          }
-        </Media>
+      <Media query={{ maxWidth: 599 }}>
+        {(matches) => (matches ? <MdHeader /> : <Header />)}
+      </Media>
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
