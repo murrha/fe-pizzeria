00519654e7fb0b6f34f0a512cadb25de191bba62
Weiyu Zhao <weiyu.zhao.2023@gmail.com>
2023-10-13 14:12:01 -0700

    update itemNotFound page

--- src/components/menuPage/NotFound.js
+++ src/components/menuPage/NotFound.js
@@ -3,12 +3,12 @@ import { useNavigate } from "react-router-dom";
 const NotFound = () => {
     let navigate=useNavigate()
     let clickHandler=()=>{
-        navigate("/")
+        navigate("/menu")
     }
     return ( 
         <div className="mx-3 mt-4">
         <h1>Item Not Found!</h1>
-        <button className="btn btn-primary mt-3" onClick={clickHandler}>Return to Home Page</button>
+        <button className="btn btn-primary mt-3" onClick={clickHandler}>Return to Main Menu</button>
         </div>
      );
 }
