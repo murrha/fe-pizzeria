64d0e597f5a2ed9e8d3286d7d36cbb4a46f3278f
murrha <murrha@outlook.com>
2023-10-17 09:37:43 -0700

    working product details page for static item

--- src/App.js
+++ src/App.js
@@ -12,7 +12,26 @@ import Media from "react-media";
 import Header from "./components/Header";
 import MdHeader from "./components/MdHeader";
 import ProductDetails from "./components/ProductDetails";
+import { useEffect, useState } from "react";
+
 function App() {
+
+  let [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await fetch("/json/products.json");
+      let data = await response.json();
+
+      setProduct(data);
+    };
+
+    console.log("Inside useEffect, App.js");
+    fetchData();
+  }, []);
+
+
+  
   return (
     <div className="container-fluid">
       <Media query={{ maxWidth: 599 }}>
@@ -24,12 +43,12 @@ function App() {
         <Route path="contact" element={<Contact />} />
         <Route path="faq" element={<FAQ />} />
         <Route path="location" element={<Location />} />
-        {/* <Route path="menu/item/:id" element={<ProductDetails products={}/>}/> */}
+        <Route path="menu/item" element={<ProductDetails productInfo={product[0]}/>}/>
         <Route path="terms" element={<TermsPage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
 
-      <ProductDetails/>
+  
 
       <Footer />
     </div>
--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,25 +1,31 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
-const ProductDetails = () => {
+const ProductDetails = (props) => {
 
-    //UseParams
-    ///menu/item/<id>
-    let productsObj = useParams(); 
-
+    console.log(props.productInfo); 
     
+    //UseParams
+    // let paramsObj = useParams(); 
+    // console.log("useParams " + paramsObj.id);
+    
+
+    //loading screen
+    if(!props.productInfo){
+        return <h2>Loading...</h2>
+    }
+
 
     return ( <>
-    
     <div className="card mx-auto w-75">
         <div className="row">
             <div className="col-md-4">
-                <img className="card-img-top" src="https://olo-images-live.imgix.net/19/192ef04f5f0b4108a0b039b70b82527b.jpg?auto=format%2Ccompress&q=60&cs=tinysrgb&w=528&h=352&fit=fill&fm=png32&bg=transparent&s=9ebfc96499020b28a2766a05edc92991" alt="Title"/>
+                <img className="card-img-top" src={`${props.productInfo.image}`} alt={`${props.productInfo.name}`}/>
             </div>
             <div className="col-md-8">
                 <div className="card-body">
-                    <h4 className="card-title">THE ORIGINAL BBQ CHICKEN PIZZA</h4>
-                    <h5>$18.99 | 190 Calories (per slice)</h5>
-                    <p className="card-text">Created here in 1985. Our legendary BBQ sauce, smoked Gouda, red onions, and fresh cilantro transform this original to iconic. Also available with Nueske's applewood smoked bacon or fresh pineapple.</p>
+                    <h4 className="card-title">{props.productInfo.name}</h4>
+                    <h5>{props.productInfo.price}</h5>
+                    <p className="card-text">{props.productInfo.description}</p>
                     <button className="btn btn-success">Add to Cart</button>
                </div>
             </div>
