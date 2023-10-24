cde3c7d70e7292e34b0bf38deb4cc511eee16b36
George Saito <143572628+gsaito2023@users.noreply.github.com>
2023-10-24 12:03:45 -0700

    Revert "Feature/t 2043 location delivery feature"

--- src/components/Location.js
+++ src/components/Location.js
@@ -15,12 +15,11 @@ const Location = () => {
           <span><b>Opening Hours:</b></span> <span>Monday to Sunday: 11am - 11pm</span>
          
           </div>
-
-          <iframe title="Google Map" className="mt-2"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50721.08080255814!2d-121.94443583138822!3d37.38823475103507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fcbff355b2f4f%3A0x7fcf3351d3ac37ff!2sSan%20Jose%2C%20CA%2095131!5e0!3m2!1sen!2sus!4v1697825665058!5m2!1sen!2sus" width="450" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+          <iframe title="Google Map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50721.08080255814!2d-121.94443583138822!3d37.38823475103507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fcbff355b2f4f%3A0x7fcf3351d3ac37ff!2sSan%20Jose%2C%20CA%2095131!5e0!3m2!1sen!2sus!4v1697825665058!5m2!1sen!2sus" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
 
           </iframe>
- 
+       
    
         </div>
 
